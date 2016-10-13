f7cb4416de6d6e33adc52e2cb95a29383c86f035
paradisebee <cbcparadisebee@gmail.com>
2016-10-13 10:57:58 +0800

    First commit

---
+++ workspace/A2/A2Part5.py
@@ -0,0 +1,35 @@
+import numpy as np
+import math
+from A2Part3 import DFT
+
+"""
+A2-Part-5: Compute the magnitude spectrum (Optional)
+
+Write a function that computes the magnitude spectrum of an input sequence x of length N. The 
+function should return an N point magnitude spectrum with frequency index ranging from 0 to N-1.
+
+The input argument to the function is a numpy array x and the function should return a numpy array of the 
+magnitude spectrum of x.
+
+EXAMPLE: If you run your function using x = np.array([1, 2, 3, 4]), the function should return the following 
+numpy array magX: [array([10.0, 2.82842712, 2.0, 2.82842712])
+"""
+def genMagSpec(x):
+    """
+    Input:
+        x (numpy array) = input sequence of length N
+    Output:
+        The function should return a numpy array
+        magX (numpy array) = The magnitude spectrum of the input sequence x
+                             (length N)
+    """
+    ## Your code here
+    
+    magX = np.array([])
+
+    N = x.size
+    X = DFT(x)
+    for n in np.arange(N):
+	magX = np.append(magX, math.sqrt(np.real(X[n])**2+np.imag(X[n])**2))
+
+    return magX
